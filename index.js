4385404f064a6c5072072e26415b1689d82b2661
EmanuelVogt <contatoemanuel3@gmail.com>
2022-07-31 23:39:30 -0300

    create first route for authenticate user

--- index.js
+++ index.js
@@ -14,3 +14,31 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
 });
+
+const API_KEY = process.env.API_KEY;
+
+app.post("/auth", (req, res) => {
+  const { username, password } = req.body;
+
+  db.query("SELECT * FROM users", (error, result) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({ status: "ERROR" });
+    }
+    const checkUser = result.find((user) => {
+      if (user.username === username && user.password === password) {
+        return user;
+      }
+    });
+
+    if (checkUser) {
+      return res.status(200).json({ status: "SUCESS", apiKey: 1 });
+    }
+
+    return res.status(401).json({ status: "UNATHORIZED" });
+  });
+});
+
+app.listen(8080, () => {
+  console.log("App listening on port 8080!");
+});
